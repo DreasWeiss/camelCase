0549ca34b91f20cc65b2f914253bc76206a58ec3
DreasWeiss <dreasweiss@gmail.com>
2023-04-08 22:18:31 +0300

    create-node-cli - starterpack

--- camelCase.js
+++ camelCase.js
@@ -1,5 +1,8 @@
 function camelCase(str) {
-
+    return str.toLowerCase().split(' ')
+        .map((i, idx) => idx > 0 ? i = `${i.slice(0, 1).toUpperCase()}${i.slice(1)}` : i)
+        .join('');
 }
 
+camelCase('');
 console.log(camelCase('Access the JSON Data from an API'));
