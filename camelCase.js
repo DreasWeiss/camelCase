847ee291fb26b60451efdec14d69e80d92a53441
DreasWeiss <dreasweiss@gmail.com>
2023-04-25 18:54:27 +0300

    feature translit + regex

--- camelCase.js
+++ camelCase.js
@@ -1,5 +1,47 @@
+const latKir = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'j',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'h',
+    'ц': 'ts',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'sht',
+    'ъ': '',
+    'ы': 'y',
+    'ь': '',
+    'э': 'e',
+    'ю': 'yu',
+    'я': 'ya'
+}
+
+function translit(str) {
+    return str.toLowerCase('').split('')
+        .map(i => latKir.hasOwnProperty(i) ? latKir[i] : i)
+        .join('');
+}
+
 function camelCase(str) {
-    return str.toLowerCase().split(' ')
+    return translit(str).replace(/[!|+|@|#|$|%|^|%|&|*|(|)|"|'|.|,|’]/g, '').toLowerCase().split(' ')
         .map((i, idx) => idx > 0 ? i = `${i.slice(0, 1).toUpperCase()}${i.slice(1)}` : i)
         .join('');
 }
